e2fcd4d73e8eb04e2a3204022a5324e29279331c
gitIt-sehyeon <tpgusdla5789@gmail.com>
2024-12-26 21:59:41 +0900

    django course 22 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 import environ
 import os
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -138,3 +141,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
