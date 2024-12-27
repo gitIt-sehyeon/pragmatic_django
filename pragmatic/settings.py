bb0040f9617abe9e106a76979f3bd00e067a26ee
gitIt-sehyeon <tpgusdla5789@gmail.com>
2024-12-27 23:28:32 +0900

    temp

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -142,5 +142,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# redirect url
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
